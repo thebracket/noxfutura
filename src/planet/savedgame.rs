0285e7f5e1cb3a9f959ec7cd02a8145d91e8a3b2
Herbert Wolverson <herberticus@gmail.com>
2020-06-03 11:39:19 -0500

    Use bincode for save/load games. MUCH faster.

--- src/planet/savedgame.rs
+++ src/planet/savedgame.rs
@@ -13,8 +13,7 @@ pub struct SavedGame {
 pub fn save_world(state: SavedGame) {
     use std::io::Write;
     let mut world_file = File::create("world.dat").unwrap();
-    let tmp = ron::to_string(&state).unwrap();
-    let mem_vec = tmp.as_bytes();
+    let mem_vec = bincode::serialize(&state).expect("Unable to binary serialize");
     let compressed_bytes = miniz_oxide::deflate::compress_to_vec(&mem_vec, 6);
     world_file.write_all(&compressed_bytes).expect("Unable to write file data");
 }
@@ -27,16 +26,11 @@ pub fn load_game() -> SavedGame {
         panic!("Saved game doesn't exist");
     }
 
-    println!("LOADFILE");
     let mut f = File::open(&savepath).expect("Unable to open file");
     let mut buffer = Vec::<u8>::new();
     f.read_to_end(&mut buffer).expect("Unable to read file");
-    println!("DECOMPRESS");
     let raw_bytes = miniz_oxide::inflate::decompress_to_vec(&buffer).expect("Unable to decompress file");
-    let s = std::str::from_utf8(&raw_bytes).unwrap().to_string();
-    println!("DESERIALIZE");
 
-    let saved: crate::planet::SavedGame = ron::from_str(&s).unwrap();
-    println!("DONE");
+    let saved: crate::planet::SavedGame = bincode::deserialize(&raw_bytes).expect("Unable to deserialize");
     saved
 }
