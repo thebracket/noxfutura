d71d4abd5e0ce91873c781ab43fdcd285a353e9c
Herbert Wolverson <herberticus@gmail.com>
2020-06-06 15:28:31 -0500

    Atlas texture kinda sorta works.

--- resources/shaders/regionblocks.frag
+++ resources/shaders/regionblocks.frag
@@ -10,13 +10,31 @@ layout(location=0) out vec4 f_color;
 layout(set = 1, binding = 0) uniform texture3D t_matinfo;
 layout(set = 1, binding = 1) uniform sampler s_matinfo;
 
-layout(set = 2, binding = 0) uniform texture2D t_terrain[212];
+layout(set = 2, binding = 0) uniform texture2D t_terrain;
 layout(set = 2, binding = 1) uniform sampler s_terrain;
 
 void main() {
     vec4 mat_tint = texture(sampler3D(t_matinfo, s_matinfo), v_world_pos);
-    //vec4 terrain_color = texture(sampler2D(t_terrain[int(mat_tint.a * 255.0)], s_terrain), v_uv);
-    vec4 tinted = mat_tint;
+    int mat_base = int(mat_tint.a * 255.0);
+    //int mat_base = 0;
+    int diffuse_tex_index = mat_base + 4; // +4
+    vec2 uv = vec2(
+        fract(v_uv.x) * 256.0,
+        fract(v_uv.y) * 256.0
+    );
+    vec2 diffuse = vec2(
+        (float(diffuse_tex_index % 16) * 256.0),
+        (float(diffuse_tex_index / 16) * 256.0)
+    );
+
+    vec2 terrain_uv = vec2(
+        (uv.x + diffuse.x) / 4096.0,
+        (uv.y + diffuse.y) / 4096.0
+    );
+
+    vec4 terrain_color = texture(sampler2D(t_terrain, s_terrain), terrain_uv);
+
+    vec4 tinted = terrain_color;
 
     vec3 lightDir = normalize(v_sun_pos - v_frag_pos);
     float diff = max(dot(lightDir, v_normal), 0.05);
--- src/modes/playgame/texarray.rs
+++ src/modes/playgame/texarray.rs
@@ -1,6 +1,12 @@
 use std::fs;
 use std::collections::HashMap;
 
+const TEXTURE_SIZE : usize = 256;
+const ATLAS_COLS : usize = 16;
+const ATLAS_STRIDE : usize = TEXTURE_SIZE * ATLAS_COLS * 4;
+const ATLAS_W : u32 = (ATLAS_COLS * TEXTURE_SIZE) as u32;
+const ATLAS_H : u32 = (ATLAS_COLS * TEXTURE_SIZE) as u32;
+
 pub struct TextureArray {
     pub texture: wgpu::Texture,
     pub view: wgpu::TextureView,
@@ -38,17 +44,30 @@ impl TextureArray {
         }
         println!("{:#?}", tex_map);
 
-        let n_images = texture_filenames.len();
+        // Make an atlas
+        let atlas_rows = 16;
+        println!("{} rows", atlas_rows);
+        let mut atlas_data = image::DynamicImage::new_rgba8(ATLAS_W, ATLAS_H);
 
+        texture_filenames.iter().enumerate().for_each(|(i, image_filename)| {
+            println!("Loading {} for Atlas, image #{}", image_filename, i);
+            let img = image::open(&image_filename).unwrap().into_rgba();
+            let x = (i % ATLAS_COLS) * TEXTURE_SIZE;
+            let y = (i / ATLAS_COLS) * TEXTURE_SIZE;
+            image::imageops::overlay(&mut atlas_data, &img, x as u32, y as u32);
+        });
+        //image::save_buffer("atlas.png", &atlas_data.raw_pixels(), ATLAS_W, ATLAS_H, image::ColorType::RGBA(8));
+
+        // Build the texture
         let size = wgpu::Extent3d {
-            width: width as u32,
-            height: height as u32,
+            width: ATLAS_W as u32,
+            height: ATLAS_H as u32,
             depth: 1,
         };
         let texture = context.device.create_texture(&wgpu::TextureDescriptor {
             label,
             size,
-            array_layer_count: n_images as u32,
+            array_layer_count: 1,
             mip_level_count: 1,
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
@@ -56,42 +75,36 @@ impl TextureArray {
             usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
         });
 
-        for (i,filename) in texture_filenames.iter().enumerate() {
-            println!("Loading {}", filename);
-            let img = image::open(&filename).unwrap().resize_exact(256, 256, image::imageops::FilterType::Lanczos3);
-            let rgba = img.into_rgba();
+        let buffer = context.device.create_buffer_with_data(&atlas_data.raw_pixels(), wgpu::BufferUsage::COPY_SRC);
 
-            let buffer = context.device.create_buffer_with_data(&rgba, wgpu::BufferUsage::COPY_SRC);
+        let mut encoder = context.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("texture_buffer_copy_encoder"),
+        });
 
-            let mut encoder = context.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: None,
-            });
+        encoder.copy_buffer_to_texture(
+            wgpu::BufferCopyView {
+                buffer: &buffer,
+                offset: 0,
+                bytes_per_row: 4 * ATLAS_W as u32,
+                rows_per_image: ATLAS_H as u32,
+            },
+            wgpu::TextureCopyView {
+                texture: &texture,
+                mip_level: 0,
+                array_layer: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            size,
+        );
 
-            encoder.copy_buffer_to_texture(
-                wgpu::BufferCopyView {
-                    buffer: &buffer,
-                    offset: 0,
-                    bytes_per_row: 4 * width as u32,
-                    rows_per_image: 1,
-                },
-                wgpu::TextureCopyView {
-                    texture: &texture,
-                    mip_level: 0,
-                    array_layer: i as u32,
-                    origin: wgpu::Origin3d::ZERO,
-                },
-                size,
-            );
-
-            let cmd_buffer = encoder.finish();
-            context.queue.submit(&[cmd_buffer]);
-        }
+        let cmd_buffer = encoder.finish();
+        context.queue.submit(&[cmd_buffer]);
 
         let view = texture.create_default_view();
         let sampler = context.device.create_sampler(&wgpu::SamplerDescriptor {
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            address_mode_u: wgpu::AddressMode::Repeat,
+            address_mode_v: wgpu::AddressMode::Repeat,
+            address_mode_w: wgpu::AddressMode::Repeat,
             mag_filter: wgpu::FilterMode::Linear,
             min_filter: wgpu::FilterMode::Nearest,
             mipmap_filter: wgpu::FilterMode::Nearest,
@@ -100,12 +113,13 @@ impl TextureArray {
             compare: wgpu::CompareFunction::Always,
         });
 
+        // Return something useful
         Ok(
             Self {
                 texture,
                 view,
                 sampler,
-                dimensions : (width, height, 1),
+                dimensions : (ATLAS_W as usize, ATLAS_H as usize, 1),
                 tex_map
             }
         )
