36ea048d4895016381b059b6a660ea99485459ba
Herbert Wolverson <herberticus@gmail.com>
2020-07-30 14:55:37 -0500

    Comments work in index file.

--- nox_raws/src/lib.rs
+++ nox_raws/src/lib.rs
@@ -48,7 +48,7 @@ impl Raws {
 
         let file = File::open("resources/raws/index.txt").unwrap();
         let reader = BufReader::new(file);
-        reader.lines().map(|l| l.unwrap()).filter(|l| !l.is_empty()).collect()
+        reader.lines().map(|l| l.unwrap()).filter(|l| !l.is_empty() && !l.starts_with("# ")).collect()
     }
 
     fn load(&mut self) {
