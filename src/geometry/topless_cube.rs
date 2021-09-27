0d807310e8b34f3c6bf2da2faf291ee1caf81cff
Herbert Wolverson <herberticus@gmail.com>
2021-09-27 10:18:25 -0500

    It's always a good idea to remember to add files to a commit.

---
+++ src/geometry/topless_cube.rs
@@ -0,0 +1,182 @@
+use super::constants::*;
+
+pub fn add_topless_cube_geometry(
+    vertices: &mut Vec<[f32; 3]>,
+    normals: &mut Vec<[f32; 3]>,
+    uv: &mut Vec<[f32; 2]>,
+    tangents: &mut Vec<[f32; 3]>,
+    x: f32,
+    y: f32,
+    z: f32,
+    w: f32,
+    h: f32,
+    d: f32,
+) {
+    let x0 = x * GEOMETRY_SIZE;
+    let x1 = (x0 + w) * GEOMETRY_SIZE;
+    let y0 = y * GEOMETRY_SIZE;
+    let y1 = (y0 + h) * GEOMETRY_SIZE;
+    let z0 = z * GEOMETRY_SIZE;
+    let z1 = (z0 + d) * GEOMETRY_SIZE;
+
+    //println!("Cube at: {},{},{}", x0, y0, z0);
+
+    #[rustfmt::skip]
+    let cube_geometry = [
+        [x0, y0, z0,],
+        [x1, y1, z0,],
+        [x1, y0, z0,],
+        [x1, y1, z0,],
+        [x0, y0, z0,],
+        [x0, y1, z0,],
+
+        [x0, y1, z1,],
+        [x0, y1, z0,],
+        [x0, y0, z0,],
+        [x0, y0, z0,],
+        [x0, y0, z1,],
+        [x0, y1, z1,],
+
+        [x1, y1, z1,],
+        [x1, y0, z0,],
+        [x1, y1, z0,],
+        [x1, y0, z0,],
+        [x1, y1, z1,],
+        [x1, y0, z1,],
+
+        [x0, y0, z0,],
+        [x1, y0, z0,],
+        [x1, y0, z1,],
+        [x1, y0, z1,],
+        [x0, y0, z1,],
+        [x0, y0, z0,],
+
+        [x1, y1, z1,],
+        [x1, y1, z0,],
+        [x0, y1, z0,],
+        [x0, y1, z0,],
+        [x0, y1, z1,],
+        [x1, y1, z1,],
+    ];
+    vertices.extend_from_slice(&cube_geometry);
+
+    #[rustfmt::skip]
+    const NORMAL_GEOMETRY: [[f32; 3]; 30] = [
+        CUBE_NORMALS[0],
+        CUBE_NORMALS[0],
+        CUBE_NORMALS[0],
+        CUBE_NORMALS[0],
+        CUBE_NORMALS[0],
+        CUBE_NORMALS[0],
+
+        CUBE_NORMALS[2],
+        CUBE_NORMALS[2],
+        CUBE_NORMALS[2],
+        CUBE_NORMALS[2],
+        CUBE_NORMALS[2],
+        CUBE_NORMALS[2],
+
+        CUBE_NORMALS[3],
+        CUBE_NORMALS[3],
+        CUBE_NORMALS[3],
+        CUBE_NORMALS[3],
+        CUBE_NORMALS[3],
+        CUBE_NORMALS[3],
+
+        CUBE_NORMALS[4],
+        CUBE_NORMALS[4],
+        CUBE_NORMALS[4],
+        CUBE_NORMALS[4],
+        CUBE_NORMALS[4],
+        CUBE_NORMALS[4],
+
+        CUBE_NORMALS[5],
+        CUBE_NORMALS[5],
+        CUBE_NORMALS[5],
+        CUBE_NORMALS[5],
+        CUBE_NORMALS[5],
+        CUBE_NORMALS[5],
+    ];
+    normals.extend_from_slice(&NORMAL_GEOMETRY);
+
+    #[rustfmt::skip]
+    const TANGENT_GEOMETRY: [[f32; 3]; 30] = [
+        CUBE_TANGENTS[0],
+        CUBE_TANGENTS[0],
+        CUBE_TANGENTS[0],
+        CUBE_TANGENTS[0],
+        CUBE_TANGENTS[0],
+        CUBE_TANGENTS[0],
+
+        CUBE_TANGENTS[2],
+        CUBE_TANGENTS[2],
+        CUBE_TANGENTS[2],
+        CUBE_TANGENTS[2],
+        CUBE_TANGENTS[2],
+        CUBE_TANGENTS[2],
+
+        CUBE_TANGENTS[3],
+        CUBE_TANGENTS[3],
+        CUBE_TANGENTS[3],
+        CUBE_TANGENTS[3],
+        CUBE_TANGENTS[3],
+        CUBE_TANGENTS[3],
+
+        CUBE_TANGENTS[4],
+        CUBE_TANGENTS[4],
+        CUBE_TANGENTS[4],
+        CUBE_TANGENTS[4],
+        CUBE_TANGENTS[4],
+        CUBE_TANGENTS[4],
+
+        CUBE_TANGENTS[5],
+        CUBE_TANGENTS[5],
+        CUBE_TANGENTS[5],
+        CUBE_TANGENTS[5],
+        CUBE_TANGENTS[5],
+        CUBE_TANGENTS[5],
+    ];
+    tangents.extend_from_slice(&TANGENT_GEOMETRY);
+
+    let tw = w;
+    let th = h;
+    #[rustfmt::skip]
+    let uv_base: [[f32; 2]; 30] = [
+        [0.0, 0.0],
+        [tw, th],
+        [tw, 0.0],
+        [tw, th],
+        [0.0, 0.0],
+        [0.0, th],
+
+        [tw, th],
+        [tw, 0.0],
+        [0.0, 0.0],
+        [0.0, 0.0],
+        [0.0, th],
+        [tw, th],
+
+        [tw, th],
+        [0.0, 0.0],
+        [tw, 0.0],
+        [0.0, 0.0],
+        [tw, th],
+        [0.0, th],
+
+        [tw, th],
+        [tw, 0.0],
+        [0.0, 0.0],
+        [0.0, 0.0],
+        [0.0, th],
+        [tw, th],
+
+        [tw, th],
+        [tw, 0.0],
+        [0.0, 0.0],
+        [0.0, 0.0],
+        [0.0, th],
+        [tw, th],
+    ];
+
+    uv.extend_from_slice(&uv_base);
+}
---
+++ src/simulation/region_builder/ramping.rs
@@ -0,0 +1,51 @@
+use crate::simulation::{
+    mapidx,
+    terrain::{
+        get_material_idx, ground_z, is_tile_floor, submit_change_batch, ChangeRequest,
+        MapChangeBatch, PlanetLocation, RampDirection,
+    },
+    REGION_HEIGHT, REGION_WIDTH,
+};
+
+pub(crate) fn build_ramps(planet_idx: PlanetLocation) {
+    let mut changes = MapChangeBatch::new(planet_idx);
+    for y in 1..REGION_HEIGHT - 1 {
+        for x in 1..REGION_WIDTH - 1 {
+            let z = ground_z(planet_idx, x, y);
+            let idx = mapidx(x, y, z);
+
+            if is_tile_floor(planet_idx, idx) {
+                if is_tile_floor(planet_idx, mapidx(x, y - 1, z + 1)) {
+                    let mat = get_material_idx(planet_idx, idx - (REGION_WIDTH * REGION_HEIGHT));
+                    changes.enqueue_change(ChangeRequest::Ramp {
+                        idx,
+                        material: mat,
+                        direction: RampDirection::NorthSouth,
+                    });
+                } else if is_tile_floor(planet_idx, mapidx(x, y + 1, z + 1)) {
+                    let mat = get_material_idx(planet_idx, idx - (REGION_WIDTH * REGION_HEIGHT));
+                    changes.enqueue_change(ChangeRequest::Ramp {
+                        idx,
+                        material: mat,
+                        direction: RampDirection::SouthNorth,
+                    });
+                } else if is_tile_floor(planet_idx, mapidx(x + 1, y, z + 1)) {
+                    let mat = get_material_idx(planet_idx, idx - (REGION_WIDTH * REGION_HEIGHT));
+                    changes.enqueue_change(ChangeRequest::Ramp {
+                        idx,
+                        material: mat,
+                        direction: RampDirection::WestEast,
+                    });
+                } else if is_tile_floor(planet_idx, mapidx(x - 1, y, z + 1)) {
+                    let mat = get_material_idx(planet_idx, idx - (REGION_WIDTH * REGION_HEIGHT));
+                    changes.enqueue_change(ChangeRequest::Ramp {
+                        idx,
+                        material: mat,
+                        direction: RampDirection::EastWest,
+                    });
+                }
+            }
+        }
+    }
+    submit_change_batch(changes);
+}
